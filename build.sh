ed94cff13e575ff2d4f629762eba0a2e5e0a8d0c
Jeff Langdon <jeff.beyer@gmail.com>
2025-03-31 21:07:40 -0700

    for deploy to pages

---
+++ build.sh
@@ -0,0 +1,1 @@
+uv run -p 3.11 python src/main.py "/boot-dev-static-site-generator/"
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 import shutil
 
@@ -25,7 +26,9 @@ def extract_title(htmlnode: HTMLNode) -> str:
     return first_element.children[0].value
 
 
-def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None:
+def generate_page(
+    from_path: Path, template_path: Path, dest_path: Path, basepath: str
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     markdown = from_path.read_text()
@@ -35,8 +38,11 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None
 
     title = extract_title(html)
 
-    template = template.replace("{{ Title }}", title).replace(
-        "{{ Content }}", html.to_html()
+    template = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html.to_html())
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
     )
 
     if not dest_path.parent.exists():
@@ -59,7 +65,7 @@ def copy_directory_recursive(src: Path, dst: Path) -> None:
 
 
 def generate_pages_recursive(
-    dir_path_content: Path, template_path: Path, dest_dir_path: Path
+    dir_path_content: Path, template_path: Path, dest_dir_path: Path, basepath: str
 ) -> None:
     if not dest_dir_path.exists():
         dest_dir_path.mkdir()
@@ -67,31 +73,36 @@ def generate_pages_recursive(
     for f in dir_path_content.iterdir():
         new_f = dest_dir_path / f.name
         if f.is_dir():
-            generate_pages_recursive(f, template_path, new_f)
+            generate_pages_recursive(f, template_path, new_f, basepath)
         else:
             if f.suffix.lower() == ".md":
-                generate_page(f, template_path, new_f.with_suffix(".html"))
+                generate_page(f, template_path, new_f.with_suffix(".html"), basepath)
             else:
                 print(f"Ignoring {f}")
 
 
 def main():
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+    print(basepath)
     project_root = Path(__file__).parent.parent
 
-    public_dir = project_root / "public"
+    dest_path = project_root / "docs"
     static_dir = project_root / "static"
 
     # remove before copying
-    if public_dir.exists():
-        shutil.rmtree(public_dir)
+    if dest_path.exists():
+        shutil.rmtree(dest_path)
 
-    copy_directory_recursive(static_dir, public_dir)
+    copy_directory_recursive(static_dir, dest_path)
 
     from_path = project_root / "content"
     template_path = project_root / "template.html"
-    dest_path = project_root / "public"
 
-    generate_pages_recursive(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
